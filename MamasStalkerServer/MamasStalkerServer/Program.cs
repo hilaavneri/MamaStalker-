7d7d7341e7ae4d7446fdfac1ed8c79e34f6882ae
Hila Avneri <hila.avneri@gmail.com>
2022-01-20 13:25:07 +0200

    created MamasStalkerServer Project

---
+++ MamasStalkerServer/MamasStalkerServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MamasStalkerServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
